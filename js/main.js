698f222440e918862d8b356908235c5b07ed9b81
ruoqinggan <ganruoqing1995@gmail.com>
2021-11-14 05:01:24 -0800

    added more features.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 var popup;
 var button;
 var tween = null;
+var has_popup = false;
 
-function debug() {
-    this.game.debug.text("Click to open window + drag + close", 32, 32);
+let mushroom_array = ['mushroom_s_brown', 'mushroom_s_red', 'mushroom_s_tan',
+    'mushroom_tall_brown', 'mushroom_tall_red', 'mushroom_tall_tan'];
+
+let mushroom_info_array = ['mushroom_info_ag_ar', 'mushroom_info_om_ol',
+    'mushroom_info_xe_su'];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
 
 function Hero(game, x, y) {
@@ -14,26 +21,13 @@ function Hero(game, x, y) {
     this.body.collideWorldBounds = true; // stay in screen
 }
 
-function openWindow() {
-    if ((tween !== null && tween.isRunning) || popup.scale.x === 1) {
-        return;
-    }
-    //  Create a tween that will pop-open the window, but only if it's not already tweening or open
-    tween = this.game.add.tween(popup.scale).to( { x: 1, y: 1 }, 1000, Phaser.Easing.Elastic.Out, true);
-}
-
-function closeWindow() {
-    if (tween && tween.isRunning || popup.scale.x === 0) {
-        return;
-    }
-    //  create a tween that will close the window, but only if it's not already tweening or closed
-    tween = this.game.add.tween(popup.scale).to( { x: 0, y: 0}, 500, Phaser.Easing.Elastic.In, true);
-}
-
 PlayState = {};
 
 // ************************ init ************************
 PlayState.init = function () {
+    this.healthCount = 5;
+    this.broccoliCount = 0;
+    this.mushroomWisdomCount = 0;
     this.game.renderer.renderSession.roundPixels = true;
     this.keys = this.game.input.keyboard.addKeys({
         left: Phaser.KeyCode.LEFT,
@@ -53,6 +47,12 @@ PlayState.preload = function () {
     this.game.load.image('background', 'images/background.png');
     // load level 1 assets
     this.game.load.json('level:1', 'data/level01.json');
+    // load health count icon
+    this.game.load.image('icon:heart_full', 'images/heart_full.png');
+    // load mushroom wisdom icon
+    this.game.load.image('icon:mushroom_wisdom', 'images/mushroom_wisdom.png');
+    // load hud values
+    this.game.load.image('font:numbers', 'images/numbers.png');
     // load images for the platforms
     this.game.load.image('ground', 'images/ground.png');
     this.game.load.image('tile:8x1', 'images/tile_8x1.png');
@@ -63,18 +63,22 @@ PlayState.preload = function () {
     // load hero image
     this.game.load.image('hero', 'images/hero_stopped.png');
     // load mushroom images
-    this.game.load.image('shroom_sb', 'images/mushroom_s_brown.png');
-    this.game.load.image('shroom_st', 'images/mushroom_s_tan.png');
-    this.game.load.image('shroom_sr', 'images/mushroom_s_red.png');
-    this.game.load.image('shroom_tb', 'images/mushroom_tall_brown.png');
-    this.game.load.image('shroom_tt', 'images/mushroom_tall_tan.png');
-    this.game.load.image('shroom_tr', 'images/mushroom_tall_red.png');
-    // load mushroom info page
-    this.game.load.spritesheet('horse-mushroom', 'images/horse_mushroom.png');
+    this.game.load.image('mushroom_s_brown', 'images/mushroom_s_brown.png');
+    this.game.load.image('mushroom_s_tan', 'images/mushroom_s_tan.png');
+    this.game.load.image('mushroom_s_red', 'images/mushroom_s_red.png');
+    this.game.load.image('mushroom_tall_brown', 'images/mushroom_tall_brown.png');
+    this.game.load.image('mushroom_tall_tan', 'images/mushroom_tall_tan.png');
+    this.game.load.image('mushroom_tall_red', 'images/mushroom_tall_red.png');
+    // load mushroom info pages
+    this.game.load.image('mushroom_info_ag_ar', 'images/mushroom_info_ag_ar.png');
+    this.game.load.image('mushroom_info_om_ol', 'images/mushroom_info_om_ol.png');
+    this.game.load.image('mushroom_info_xe_su', 'images/mushroom_info_xe_su.png');
     // load sound effect for jumping
     this.game.load.audio('sfx:jump', 'audio/jump.wav');
     // load sound effect for eating a broccoli
     this.game.load.audio('sfx:broccoli', 'audio/broccoli.wav');
+    // load sound effect for eating a mushroom
+    this.game.load.audio('sfx:mushroom', 'audio/mushroom.wav');
     // load broccoli image
     this.game.load.spritesheet('broccoli', 'images/broccoli_animated.png', 22, 22);
     // load close button images
@@ -105,8 +109,37 @@ PlayState.create = function () {
     // create sound entities
     this.sfx = {
         jump: this.game.add.audio('sfx:jump'),
-        broccoli: this.game.add.audio('sfx:broccoli')
+        broccoli: this.game.add.audio('sfx:broccoli'),
+        mushroom: this.game.add.audio('sfx:mushroom')
     };
+
+    //create hud
+    this._createHud();
+};
+
+PlayState._createHud = function () {
+    let healthIcon = this.game.make.image(0, 0, 'icon:heart_full');
+    let wisdomIcon = this.game.make.image(0, 40, 'icon:mushroom_wisdom');
+    this.hud = this.game.add.group();
+    this.hud.add(healthIcon);
+    this.hud.add(wisdomIcon);
+    this.hud.position.set(10, 10);
+
+    const NUMBERS_STR = '0123456789X ';
+    this.healthFont = this.game.add.retroFont('font:numbers', 20, 26,
+        NUMBERS_STR, 6);
+    this.wisdomFont = this.game.add.retroFont('font:numbers', 20, 26,
+        NUMBERS_STR, 6);
+
+    let healthScoreImg = this.game.make.image(healthIcon.x + healthIcon.width,
+        healthIcon.height / 2, this.healthFont);
+    healthScoreImg.anchor.set(0, 0.5);
+    let wisdomScoreImg = this.game.make.image(wisdomIcon.x + wisdomIcon.width,
+        wisdomIcon.height * 1.5, this.wisdomFont);
+    wisdomScoreImg.anchor.set(0, 0.5);
+
+    this.hud.add(healthScoreImg);
+    this.hud.add(wisdomScoreImg);
 };
 
 PlayState._loadLevel = function (data) {
@@ -114,38 +147,18 @@ PlayState._loadLevel = function (data) {
     this.platforms = this.game.add.group();
     // create broccolis
     this.broccolis = this.game.add.group();
+    // create mushrooms
+    this.mushrooms = this.game.add.group();
     // spawn all platforms
     data.platforms.forEach(this._spawnPlatform, this);
     // spawn hero and enemies
     this._spawnCharacters({hero: data.hero});
     // spawn important objects
     data.broccolis.forEach(this._spawnBroccoli, this);
+    data.mushrooms.forEach(this._spawnMushrooom, this);
     // enable gravity
     const GRAVITY = 1200;
     this.game.physics.arcade.gravity.y = GRAVITY;
-
-    // create pop-up window
-    popup = this.game.add.sprite(this.game.world.centerX,
-        this.game.world.centerY, 'horse-mushroom');
-    popup.anchor.set(0.5);
-    popup.inputEnabled = true;
-    popup.input.enableDrag();
-    //  Position the close button to the top-right of the popup sprite (minus 8px for spacing)
-    var pw = (popup.width / 2) - 30;
-    var ph = (popup.height / 2) - 8;
-
-    //  And click the close button to close it down again
-    var closeButton = this.game.make.sprite(pw, -ph, 'close');
-    closeButton.inputEnabled = true;
-    closeButton.input.priorityID = 1;
-    closeButton.input.useHandCursor = true;
-    closeButton.events.onInputDown.add(closeWindow, this);
-
-    //  Add the "close button" to the popup window image
-    popup.addChild(closeButton);
-
-    //  Hide it awaiting a click
-    popup.scale.set(0);
 };
 
 // spawn the platforms
@@ -171,8 +184,57 @@ PlayState._spawnBroccoli = function (broccoli) {
     sprite.body.allowGravity = false;
 }
 
+// spawn mushrooms
+PlayState._spawnMushrooom = function (mushroom) {
+    let sprite = this.mushrooms.create(mushroom.x, mushroom.y, mushroom.image);
+    this.game.physics.enable(sprite);
+    sprite.body.allowGravity = false;
+}
+
+PlayState._createPopup = function(mushroom_info) {
+    // create pop-up window
+    popup = this.game.add.sprite(this.game.world.centerX,
+        this.game.world.centerY, mushroom_info);
+    popup.anchor.set(0.5);
+    popup.inputEnabled = true;
+    popup.input.enableDrag();
+    //  Position the close button to the top-right of the popup sprite (minus 8px for spacing)
+    var pw = (popup.width / 2) - 30;
+    var ph = (popup.height / 2) + 10;
+
+    //  And click the close button to close it down again
+    var closeButton = this.game.make.sprite(pw, -ph, 'close');
+    closeButton.inputEnabled = true;
+    closeButton.input.priorityID = 1;
+    closeButton.input.useHandCursor = true;
+    closeButton.events.onInputDown.add(this._closeWindow, this);
+
+    //  Add the "close button" to the popup window image
+    popup.addChild(closeButton);
+
+    //  Hide it awaiting a click
+    popup.scale.set(0);
+}
+
+PlayState._openWindow = function() {
+    if ((tween !== null && tween.isRunning) || popup.scale.x === 1) {
+        return;
+    }
+    //  Create a tween that will pop-open the window, but only if it's not already tweening or open
+    tween = this.game.add.tween(popup.scale).to( { x: 1, y: 1 }, 1000, Phaser.Easing.Elastic.Out, true);
+}
+
+PlayState._closeWindow = function() {
+    if (tween && tween.isRunning || popup.scale.x === 0) {
+        return;
+    }
+    //  create a tween that will close the window, but only if it's not already tweening or closed
+    tween = this.game.add.tween(popup.scale).to( { x: 0, y: 0}, 500, Phaser.Easing.Elastic.In, true);
+    has_popup = false;
+}
+
 window.onload = function () {
-    let game = new Phaser.Game(960, 600, Phaser.AUTO, 'fungifun');
+    let game = new Phaser.Game(960, 600, Phaser.AUTO, 'findingfungi');
     game.state.add('play', PlayState);
     game.state.start('play');
 };
@@ -181,9 +243,15 @@ window.onload = function () {
 PlayState.update = function () {
     this._handleCollisions();
     this._handleInput();
+    this.healthFont.text = `x${this.healthCount}`;
+    this.wisdomFont.text = `x${this.mushroomWisdomCount}`;
 };
 
 PlayState._handleInput = function () {
+    if (has_popup) {
+        this.hero.move(0);
+        return;
+    }
     if (this.keys.left.isDown) { // move hero left
         this.hero.move(-1);
     }
@@ -198,17 +266,33 @@ PlayState._handleInput = function () {
 PlayState._handleCollisions = function () {
     this.game.physics.arcade.collide(this.hero, this.platforms);
     this.game.physics.arcade.overlap(this.hero, this.broccolis,
-        this._onHeroVsBrocolli, null, this);;
+        this._onHeroVsBrocolli, null, this);
+    this.game.physics.arcade.overlap(this.hero, this.mushrooms,
+        this._onHeroVsMushroom, null, this);
 };
 
+// hero collides with broccolis
 PlayState._onHeroVsBrocolli = function (hero, broccoli) {
+    ++this.broccoliCount;
+    if (this.broccoliCount <= 5) {
+        ++this.healthCount;
+    } else if (this.broccoliCount % 5 === 0) {
+        --this.healthCount;
+    }
     this.sfx.broccoli.play();
     broccoli.kill();
+};
 
-    if ((tween !== null && tween.isRunning) || popup.scale.x === 1) {
-        return;
+// hero collides with mushrooms
+PlayState._onHeroVsMushroom = function (hero, mushroom) {
+    ++this.mushroomWisdomCount;
+    this.sfx.mushroom.play();
+    mushroom.kill();
+    has_popup = true;
+    var int = getRandomInt(3);
+    this._createPopup(mushroom_info_array[int]);
+    if (int === 1) {
+        this.healthCount -= 5;
     }
-    //  Create a tween that will pop-open the window, but only if it's not already tweening or open
-    tween = this.game.add.tween(popup.scale).to( { x: 1, y: 1 }, 1000, Phaser.Easing.Elastic.Out, true);
-
+    this._openWindow();
 };
